dc8ec00ea58f494e8ba26b518466c32b07020dd9
Reg Tiangha <rtiangha@users.noreply.github.com>
2024-04-12 22:09:07 -0600

    Customize build options

--- Source/Script/Build.sh
+++ Source/Script/Build.sh
@@ -2,7 +2,7 @@
 set -ex
 script_dir="$(dirname "$0")"
 toplvl_dir="$(realpath "$script_dir/../../")"
-build_config="${1:-Debug}"
+build_config="${1:-Release}"
 build_dir="$toplvl_dir/Build/$build_config"
 threads="${2:-$(nproc)}"
 
@@ -15,7 +15,7 @@ fi
 
 mkdir -p "$build_dir"
 
-cmake -S "$toplvl_dir" -B "$build_dir" -DCMAKE_BUILD_TYPE="$build_config" -DPORTABLE_INSTALL=ON -DUSE_ANGRYLION=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -G "Ninja"
+cmake -S "$toplvl_dir" -B "$build_dir" -DCMAKE_BUILD_TYPE="$build_config" -DCMAKE_CXX_FLAGS="-march=skylake -Ofast" -DCMAKE_C_FLAGS="-march=skylake -Ofast" -DPORTABLE_INSTALL=ON -DUSE_ANGRYLION=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -G "Ninja"
 
 cmake --build "$build_dir" --parallel "$threads"
 
